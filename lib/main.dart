ae29f3d5ddc0120b36445dd34eed0f7c92e6bd07
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-06-16 23:01:04 +0800

    v2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,119 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(IAmRane());
+}
+
+class IAmRane extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey[850],
         appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: Center(
-            child: Text("I am Rane"),
-          ),
+          title: Text('I AM RANE'),
+          backgroundColor: Colors.blue[800],
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(bottom: 100.0),
+        backgroundColor: Colors.grey[850],
+        body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Center(
-                child: Image(
-                  image: AssetImage('images/ME_cartoon.png'),
+              Container(
+                width: double.infinity,
+              ),
+              CircleAvatar(
+                radius: 70.0,
+                backgroundColor: Colors.blue[400],
+                backgroundImage: AssetImage("images/ME_cartoon.png"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'RANE',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                    ),
+                  ),
+                  Text(
+                    'GILLIAN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.normal,
+                      fontSize: 28.0,
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15.0,
+                  fontFamily: 'Montserrat',
                 ),
               ),
-              Center(
+              SizedBox(
+                height: 20.0,
+                width: 131.0,
+                child: Divider(color: Colors.grey[200]),
+              ),
+              Card(
+                color: Colors.grey[700],
+                margin: EdgeInsets.fromLTRB(25.0, 2.0, 25.0, 10.0),
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    'I am Rane Gillian Villanueva. That\'s it.',
-                    style: TextStyle(color: Colors.white),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.grey[200],
+                        size: 16.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('rgvillanueva28@gmail.com',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Colors.grey[200],
+                            fontSize: 16.0,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.grey[700],
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.grey[200],
+                        size: 16.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('09196993475',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Colors.grey[200],
+                            fontSize: 16.0,
+                          )),
+                    ],
                   ),
                 ),
               ),
@@ -35,6 +121,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
